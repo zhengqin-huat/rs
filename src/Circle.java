e99499ff758f175e57e170950317bb5c81ed19dc
thresh_q <zhengqin_huat@163.com>
2020-05-10 13:46:02 +0800

    fct mod

---
+++ src/Circle.java
@@ -0,0 +1,5 @@
+public class Circle  implements shape{
+    public void drow() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,6 @@
+public class Rectangle implements shape {
+    @Override
+    public void drow() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
