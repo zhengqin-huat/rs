d5d4c97bce9eadd3050e277aa6e8ad3aece23d4b
thresh_q <zhengqin_huat@163.com>
2020-05-11 19:46:28 +0800

    fct mod

---
+++ src/cons/SelectionSort.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package cons;
+
+import java.util.Arrays;
+
+/**
+ *选择排序
+ */
+public class SelectionSort {
+
+
+	public static void swapElements(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+
+	public static int indexLowest(int[] array, int start) {
+		int lowIndex = start;
+		for (int i = start; i < array.length; i++) {
+			if (array[i] < array[lowIndex]) {
+				lowIndex = i;
+			}
+		}
+		return lowIndex;
+	}
+
+
+	public static void selectionSort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int j = indexLowest(array, i);
+			swapElements(array, i, j);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = {2, 5, 6, 1, 3};
+		selectionSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+}
