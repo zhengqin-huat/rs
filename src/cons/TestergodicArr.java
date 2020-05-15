f77c495ce22b8353aa330288f7c326947893ec82
thresh_q <zhengqin_huat@163.com>
2020-05-15 11:18:21 +0800

    fct mod

---
+++ src/cons/TestergodicArr.java
@@ -0,0 +1,58 @@
+package cons;
+import java.util.*;
+
+public class TestergodicArr {
+
+
+        public static void main(String[] args) {
+            List<String> list=new ArrayList<String>();
+            list.add("Hello");
+            list.add("World");
+            list.add("HAHAHAHA");
+
+            for (String str : list) {
+                System.out.println(str);
+            }
+
+
+            String[] strArray=new String[list.size()];
+            list.toArray(strArray);
+            for(int i=0;i<strArray.length;i++)
+            {
+                System.out.println(strArray[i]);
+            }
+
+
+
+            Iterator<String> ite=list.iterator();
+            while(ite.hasNext())
+            {
+                System.out.println(ite.next());
+            }import java.util.*;
+
+            public class Test{
+                public static void main(String[] args) {
+                    List<String> list=new ArrayList<String>();
+                    list.add("Hello");
+                    list.add("World");
+                    list.add("HAHAHAHA");
+                    //第一种遍历方法使用 For-Each 遍历 List
+                    for (String str : list) {
+                        System.out.println(str);
+                    }
+
+
+                    String[] strArray=new String[list.size()];
+                    list.toArray(strArray);
+                    for(int i=0;i<strArray.length;i++)
+                    {
+                        System.out.println(strArray[i]);
+                    }
+
+
+                    Iterator<String> ite=list.iterator();
+                    while(ite.hasNext())
+                    {
+                        System.out.println(ite.next());
+                    }
+}
