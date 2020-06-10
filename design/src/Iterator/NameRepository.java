8df55c17b6cfbfca35f790ee010639bfcc7bb643
thresh_q <zhengqin_huat@163.com>
2020-06-10 21:17:20 +0800

    fct mod

---
+++ design/src/Iterator/NameRepository.java
@@ -0,0 +1,32 @@
+package Iterator;
+
+public class NameRepository implements Container {
+
+    public String names[] = {"No1" , "No2" ,"No1" , "No2"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
